aaa69a6210561587e32ab8006d114ccb83f7f874
Danny Brandt <dvbrandt90@gmail.com>
2021-01-07 01:47:17 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -20,7 +20,7 @@ class CashRegister
   end
 
   def apply_discount
-    puts total.to_f
+    puts @total.to_f
     total = @total.to_f-20
     discount = @discount.to_f / 100
     reduce = total * discount
