3f1a84a8268183a33e290c9f771d52c36c466633
Danny Brandt <dvbrandt90@gmail.com>
2021-01-07 01:40:22 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -23,7 +23,7 @@ class CashRegister
     total = @total.to_f - 20
     discount = @discount.to_f / 100
     reduce = total * discount
-    if discount == 0 
+    if @discount == 0 
     return "There is no discount to apply."
     else
       @total = total - reduce
