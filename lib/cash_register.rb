9419d652a6a2224be0fb7a370629b2d9f7ccbb10
Danny Brandt <dvbrandt90@gmail.com>
2021-01-07 01:02:49 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,1 +1,31 @@
+class CashRegister
+  attr_accessor :total, :discount
+  
+  def initialize(total = 0, discount = 20)
+    @discount = discount
+    @total = total
+  end
+  
+  def total
+    @total
+  end
+  
+  def add_item(title, price, quantity = 0)
+    if quantity > 0
+      price = price * quantity
+    end
+    total_dollars = @total.to_f 
+    total_dollars += price
+    @total = total_dollars
+  end
 
+  def apply_discount
+    total = @total.to_f
+    discount = @discount.to_f
+    puts discount
+    puts total
+     apply = (100 - discount)/100
+     puts total * apply
+    return total * apply
+  end
+end
